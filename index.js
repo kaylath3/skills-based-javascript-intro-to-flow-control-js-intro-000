914075f798f8e9d5ef01e099c0805039f42bc139
kaylathoma <kaylath3@gmail.com>
2016-11-02 16:35:22 +0000

    Second to last code from nitrous

---
+++ index.js
@@ -0,0 +1,24 @@
+// write your code below!
+
+function basicTeenager(age) {
+  if (age >= 13 & age <= 19) {
+
+  } else {
+
+  }
+}
+function teenager(age) {
+
+}
+
+function ageChecker(age) {
+
+}
+
+function ternaryTeenager(age) {
+
+}
+
+function switchAge(age) {
+
+}
